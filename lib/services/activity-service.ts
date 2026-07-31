9db5a8d35502773d8b532e9f4058d1770bad7553
Kadir Gürlü <kadirkgurlu@gmail.com>
2026-07-31 15:29:18 +0300

    Delete lib directory

--- lib/services/activity-service.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { readLearningState } from "@/lib/storage/learning-storage";
-
-export function getRecentActivities(limit = 10) {
-  return readLearningState().activities.slice(0, limit);
-}
